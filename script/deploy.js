39fd3ed5b231d22058332f4f75b99a2fa09c799a
Brendan Graetz <bguiz@users.noreply.github.com>
2026-07-02 17:18:48 +0800

    fix: resolve issues with deployment script
    Signed-off-by: Brendan Graetz <bguiz@users.noreply.github.com>

--- script/deploy.js
+++ script/deploy.js
@@ -6,10 +6,30 @@ async function main() {
     gasPrice: 160e6,
     gasLimit: 2e6,
   });
+  console.log('Deploying Counter...');
 
-  await counter.waitForDeployment();
-
-  console.log('Counter deployed to:', await counter.getAddress());
+  // Note that Injective has **too fast** a block time (< 1s)
+  // for ethers' `contract.waitForDeployment()`.
+  // By the time it polls, the block in which the deployment transaction
+  // was mined is already in the rear-view mirror.
+  // We'll implement our own custom polling in lieu of this.
+  const deployTx = counter.deploymentTransaction();
+  console.log('Deployment transaction:', deployTx);
+  let attempts = 3;
+  let deployTxReceipt;
+  while ((attempts--) > 0) {
+    await new Promise((resolve) => setTimeout(resolve, 700));
+    deployTxReceipt = await deployTx.wait(0);
+    if (!!deployTxReceipt) {
+      break;
+    }
+  }
+  console.log('Deployment transaction receipt:', deployTxReceipt);
+  if (deployTxReceipt && deployTxReceipt.status === 1) {
+    console.log('Counter deployed to:', await counter.getAddress());
+  } else {
+    console.log('Counter deployment failed. (Transaction failed, or receipt not found.)');
+  }
 }
 
 main().catch((error) => {
